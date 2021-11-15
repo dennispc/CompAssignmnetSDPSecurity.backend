02f97de6bc5b63e4e89f8c9962bb426837527a18
jbn108 <jbuus9@gmail.com>
2021-11-15 17:47:21 +0100

    ProductController set up + more testing

--- quertygroup.WebApi.Test/ProductControllerTest.cs
+++ quertygroup.WebApi.Test/ProductControllerTest.cs
@@ -1,11 +1,14 @@
 
 
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using CompAssignmnetSDPSecurity.Core.Models;
+using CompAssignmnetSDPSecurity.Core.Services;
 using CompAssignmnetSDPSecurity.WebApi.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Xunit;
 
 namespace quertygroup.WebApi.Test
@@ -13,9 +16,10 @@ namespace quertygroup.WebApi.Test
     public class ProductControllerTest
     {
         [Fact]
-        public void ProductController_IsOfTypeControllerBare()
+        public void ProductController_IsOfTypeControllerBase()
         {
-            var controller = new ProductController();
+            var service = new Mock<IProductService>();
+            var controller = new ProductController(service.Object);
             Assert.IsAssignableFrom<ControllerBase>(controller);
         }
 
@@ -62,6 +66,40 @@ namespace quertygroup.WebApi.Test
             var method = typeof(ProductController)
                 .GetMethods().FirstOrDefault(m => "GetAll".Equals(m.Name));
             Assert.Equal(typeof(ActionResult<List<Product>>).FullName, method.ReturnType.FullName);
+        }
+
+        [Fact]
+        public void GetAll_WithNoParams_HasGetHttpAttribute()
+        {
+            var methodInfo = typeof(ProductController)
+                .GetMethods()
+                .FirstOrDefault(m => m.Name == "GetAll");
+
+            var attr = methodInfo.CustomAttributes
+                .FirstOrDefault(ca => ca.AttributeType.Name == "HttpGetAttribute");
+            
+            Assert.NotNull(attr);
+        }
+
+        [Fact]
+        public void ProductController_HasProductService_IsOfTypeControllerBare()
+        {
+            
+        }
+        
+        [Fact]
+        public void ProductController_WithNullProductService_ThrowsInvalidDataException()
+        {
+            Assert.Throws<InvalidDataException>(() =>
+                new ProductController(null));
+        }
+        
+        [Fact]
+        public void ProductController_WithNullProductService_ThrowsInvalidDataExceptionWithMessage()
+        {
+            var exception = Assert.Throws<InvalidDataException>(() =>
+                new ProductController(null));
+            Assert.Equal("ProductService cannot be null", exception.Message);
         }
     }
 }
